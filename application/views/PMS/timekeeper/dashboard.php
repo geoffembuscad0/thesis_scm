55bd5836c384e79059c42acbceec8d2d40df3d48
Geoffrey Embuscado <geoff.embuscado@yahoo.com.ph>
2014-01-29 12:55:20 +0800

    updated app on jan 29 2014

--- application/views/PMS/timekeeper/dashboard.php
+++ application/views/PMS/timekeeper/dashboard.php
@@ -1,6 +1,5 @@
 <?php echo $head; ?>
 <?php echo $page_header; ?>
-<?php //echo $verify_applicant_form; ?>
 <div class="pure-g" style="text-align:left;">
     <div class="pure-u-1-3" style="background:#6F4E37;color:white;width:50%;">
     	<h1>Dashboard</h1>
@@ -18,7 +17,7 @@
 					<th>Employee</th>
 					<th>Position</th>
 					<th>Department</th>
-					<th>Hours Worked</th>
+					<th>Hours</br>Worked</th>
 					<th>Action</th>
 				</tr>
 			</thead>
@@ -26,12 +25,14 @@
 			<?php foreach($employees AS $employee){?>
 				<tr>
 					<td><?php echo $employee['employee_id']; ?></td>
-					<td><?php echo $employee['lastname'] . ', ' .$employee['firstname'];?></td>
+					<td style="text-align: left; padding-left:10px;">
+						<?php echo $employee['lastname'] . ', ' .$employee['firstname'] . ' ' . ucfirst(substr($employee['middlename'], 0,1)) . '.';?>
+					</td>
 					<td><?php echo $employee['pos_name'];?></td>
 					<td><?php echo $employee['department'];?></td>
 					<td><?php echo round($employee['total_hours']);?></td>
 					<td>
-					<button id="showEmployeeLogs<?php echo $employee['employee_id'];?>" class="approve">View Employee Logs</button>
+					<button id="showEmployeeLogs<?php echo $employee['employee_id'];?>" class="approve">Employee Logs</button>
 					<button class="approve" onClick="self.location='<?php echo URL::site('pms/print_payslip?employee_id='.$employee['employee_id']);?>'">Print Payslip</button>
 					<div id="employeeLogs<?php echo $employee['employee_id'];?>" class="reveal-modal">
 						<p class="close-reveal-modal">close[x]</p>
--- application/views/PMS/timekeeper/payslip.php
+++ application/views/PMS/timekeeper/payslip.php
@@ -1,70 +1,83 @@
 <style type='text/css'>
-*{ text-align:center; }
-.right { text-align: right;}
-table {width:100%;border: 1px solid #000;}
+*{ text-align: center;}
+.right{ text-align: right;}
+table { border: 1px solid #000; }
 table tr {border: 1px solid #000;}
 table tr th,td{border: 1px solid #000;padding: 6px 6px 6px 6px;text-align:left;}
 </style>
 <page>
-<h1>Stellar Cargo Movers</h1><h2>Payroll Payslip</h2>
+<h1>Teresa Marble Corporation</h1>
+<h2>Payroll Payslip</h2>
 <h3 class='right'>Date: <?php echo $date;?></h3>
+
 <table>
-
 <tr>
-<th>Employee ID:</th><td colspan='2'><?php echo $employee_id;?></td>
-<th>Employee Name:</th><td colspan='4'><?php echo $employee_name;?></td>
+<th colspan='7'>Employee Information</th>
 </tr>
 
 <tr>
-<th>Department:</th>
-<td colspan='3'><?php echo $employee_department;?></td>
-<th>Employee Position:</th>
-<td colspan='3'><?php echo $employee_position;?></td>
+<td>Employee ID:</td><td><?php echo $employee_id;?></td>
+<td>Employee Name:</td><td colspan='4'><?php echo $employee_name;?></td>
 </tr>
 
 <tr>
-<th colspan='2'>Employee Type:</th>
-<td colspan='3'><?php echo $employee_type;?></td>
-<th>Marital Status</th>
-<td colspan='2'><?php echo $marital_status;?></td>
+<td>Department:</td>
+<td><?php echo $employee_department;?></td>
+<td>Employee Position:</td>
+<td colspan='4'><?php echo $employee_position;?></td>
 </tr>
 
 <tr>
-<th>No. of days worked:</th>
+<td>Employee Type:</td>
+<td><?php echo $employee_type;?></td>
+<td>Marital Status</td>
+<td colspan=4'><?php echo $marital_status;?></td>
+</tr>
+
+<tr>
+<th colspan='7'>Salary Information</th>
+</tr>
+
+<tr>
+<td>No. of days <br>worked:</td>
+<td>Total Hours <br>Worked:</td>
+<td>OT Rate:</td>
+<td>Total <br>OT Hours:</td>
+<td>Basic Salary:</td>
+<td>GROSS Pay:</td>
+<td>NET Pay:</td>
+</tr>
+
+<tr>
 <td><?php echo $days_worked;?></td>
-<th>Rate per Hour:</th>
-<td><b>PHP <?php echo number_format($employee_rate,2);?></b></td>
-<th>Total Hours Worked:</th>
 <td><?php echo round($total_hours_worked);?></td>
-<th>OT Hours:</th>
+<td>PHP <?php echo $OT_rate;?></td>
 <td><?php echo $OT_hours;?></td>
+<td>PHP <?php echo number_format($basic_salary,2,'.',',');?></td>
+<td>PHP <?php echo number_format($gross_pay,2,'.',',');?></td>
+<td>PHP <?php echo number_format($net_value,2,'.',',');?></td>
 </tr>
 
 <tr>
-<th>Basic Salary:</th>
-<td colspan='2'><b>PHP <?php echo number_format($basic_salary,2,'.',',');?></b></td>
-<th>OT Rate:</th>
-<th>PHP <?php echo $OT_rate;?></th>
-<th>GROSS Pay:</th>
-<th colspan='2'><b>PHP <?php echo number_format($gross_pay,2,',','.');?></b></th>
+<th colspan='7'>Deduction Informations</th>
 </tr>
 
 <tr>
-<th>Deductions:</th>
-<td colspan='4'>
 <?php foreach($deductions AS $deduction_label => $deduction_val){ ?>
-	<?php echo $deduction_label . ": PHP" . $deduction_val;?>
+<?php echo "<td>".$deduction_label."</td>";?>
 <?php } ?>
-</td>
-<th>NET Pay:</th>
-<td colspan='2'>PHP <?php echo number_format($net_value,2,'.',',');?></td>
+<td>Total Deduction</td>
+<td>BIR Excess</td>
+<td colspan='2'>BIR Tax</td>
 </tr>
 
-
 <tr>
-<th>Misc Additional</th>
-<td colspan='7'>[adds PHP <?php echo $employee_single_married_twenty;?> if employee is married]</td>
+<?php foreach($deductions AS $deduction_label => $deduction_val){ ?>
+<?php echo "<td>PHP ".$deduction_val."</td>";?>
+<?php } ?>
+<td><?php echo $total_deductions;?></td>
+<td>PHP <?php echo $bir_excess;?></td>
+<td colspan='2'>PHP <?php echo $bir_tax;?></td>
 </tr>
 </table>
-
 </page>
